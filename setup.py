50be86ae7e9089e7c603ed7ef5f41d4190f06ae3
Daniel Munch <30357821+Dmunch04@users.noreply.github.com>
2019-03-03 00:11:02 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import multiprocessing
+from setuptools import setup
+
+setup(name='eve',
+    version='1.0',
+    py_modules=['eve'],
+    author='Munchii',
+    author_email='bocongyin@gmail.com',
+    license='MIT',
+    description='Simple lightweight data-interchange format, written in Python',
+    url='https://github.com/Dmunch04/EVE',
+    classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: End Users/Desktop",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+        ],
+    keywords='simple data interchange format python',
+    )
