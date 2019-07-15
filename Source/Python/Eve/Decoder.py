4c4e6c2c126d8a6aa24a7e2ccba7654c2972463e
Daniel <daniellmunch@gmail.com>
2019-07-15 18:21:01 +0200

    Made the code better

--- Source/Python/Eve/Decoder.py
+++ Source/Python/Eve/Decoder.py
@@ -1,4 +1,5 @@
 import os
+from DavesLogger import Logs
 
 from Eve.Helpers.Helper import CheckValue, Keywords, OptionWords
 
@@ -13,11 +14,7 @@ def Load (_Path: str, **_Options) -> dict:
         return Loads (File.read (), **_Options)
 
 def Loads (_Data: str, **_Options) -> dict:
-    if isinstance (_Data, str):
-        Data = _Data.split ('\n')
-
-    else:
-        Data = _Data
+    Data = _Data.split ('\n') if isinstance (_Data, str) else _Data
 
     StartFound = False
     EndFound = False
@@ -28,10 +25,10 @@ def Loads (_Data: str, **_Options) -> dict:
     ToApply = ''
 
     for Line in Data:
-        if Line in ' \n\t':
+        if Line.strip () in ' \n\t':
             continue
 
-        FixedLine = Line.replace (' ', '').replace ('\n', '').replace ('\t', '')
+        FixedLine = Line.replace (' ', '').replace ('\n', '').replace ('\t', '').strip ()
 
         if FixedLine.startswith ('@'):
             continue
@@ -61,38 +58,39 @@ def Loads (_Data: str, **_Options) -> dict:
             Key = Splitted[0][1 : len (Splitted[0]) - 1]
             Value = Splitted[1].split ('@')[0]
 
+            # It's a list
             if Value.startswith ('(') and Value.endswith (')'):
                 Values = Value[1 : len (Value) - 1].split (',')
 
-                FinishedValues = []
-                for XValue in Values:
-                    XValue = CheckValue (XValue, CurrentSectionData)
+                Elements = []
+                for Element in Values:
+                    Element = CheckValue (Element, CurrentSectionData)
 
-                    FinishedValues.append (XValue)
+                    Elements.append (Element)
 
                 if ToApply:
                     Apply = ToApply.lower ().split ('.')
-                    if Apply[0] in Keywords:
-                        if Apply[1] in OptionWords[Apply[0]]:
-                            FinishedValues = OptionWords[Apply[0]][Apply[1]] (FinishedValues)
-                            ToApply = ''
+                    if Apply[0] in Keywords and Apply[1] in OptionWords[Apply[0]]:
+                        Elements = OptionWords[Apply[0]][Apply[1]] (Elements)
+                        ToApply = ''
 
-                CurrentSectionData[Key] = FinishedValues
+                CurrentSectionData[Key] = Elements
 
+            # It's not a list
             else:
                 if ToApply:
                     Apply = ToApply.lower ().split ('.')
-                    if Apply[0] in Keywords:
-                        if Apply[1] in OptionWords[Apply[0]]:
-                            Value = f'{OptionWords[Apply[0]][Apply[1]] (Value)}'
-                            ToApply = ''
+
+                    if Apply[0] in Keywords and Apply[1] in OptionWords[Apply[0]]:
+                        Value = str ({OptionWords[Apply[0]][Apply[1]] (Value)})
+                        ToApply = ''
 
                 Value = CheckValue (Value, CurrentSectionData)
 
                 CurrentSectionData[Key] = Value
 
     if SectionIndex == -1:
-        print ('No section detected! (\'[]\')')
+        Logs.Error ('No section detected! (\'[]\')')
 
         return
 
@@ -101,5 +99,5 @@ def Loads (_Data: str, **_Options) -> dict:
 
     else:
         Content.append (Sections)
-        
+
         return Content[0]
